b192e44d8a7c10136d19444fa9f5ea7e3c039797
Reilly Forshaw <reilly.forshaw@gmail.com>
2018-03-29 08:25:20 -0700

    Initial Commit

---
+++ JSONQuery/JSONQuery.h
@@ -0,0 +1,19 @@
+//
+//  JSONQuery.h
+//  JSONQuery
+//
+//  Created by Reilly Forshaw on 2018-03-29.
+//  Copyright © 2018 Reilly Forshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONQuery.
+FOUNDATION_EXPORT double JSONQueryVersionNumber;
+
+//! Project version string for JSONQuery.
+FOUNDATION_EXPORT const unsigned char JSONQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONQuery/PublicHeader.h>
+
+
